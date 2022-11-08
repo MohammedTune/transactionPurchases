145dc8a1d4e4170267764a804e51ffca1291d8b5
mohammed <mtune37@gmail.com>
2022-11-09 00:27:27 +0300

    purchase_Transaction

---
+++ src/main/java/com/shamaProject/demoApp/Getmapping.java
@@ -0,0 +1,2 @@
+package com.shamaProject.demoApp;public @interface Getmapping {
+}
---
+++ src/test/java/com/shamaProject/demoApp/DemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shamaProject.demoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
